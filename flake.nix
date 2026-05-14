dee4be3749e95c831728df871d6d38f2146530d0
Peter Marreck <lumbergh@gmail.com>
2026-05-14 19:02:44 -0400

    flake: pin Zig 0.16.0 via mitchellh/zig-overlay (fix CI on Garnix)

--- flake.nix
+++ flake.nix
@@ -4,16 +4,21 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    zig-overlay = {
+      url = "github:mitchellh/zig-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     blip = {
       url = "github:pmarreck/BLIP/v3.0.0";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, flake-utils, blip, ... }:
+  outputs = { self, nixpkgs, flake-utils, zig-overlay, blip, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+        zig = zig-overlay.packages.${system}."0.16.0";
         pname = "mini_blar";
         version = "3.0.0";
         isDarwin = pkgs.stdenv.isDarwin;
@@ -44,7 +49,7 @@
           dontFixup = true;
         };
 
-        commonInputs = [ pkgs.zig ]
+        commonInputs = [ zig ]
           ++ pkgs.lib.optionals isDarwin [
             pkgs.darwin.cctools
             pkgs.apple-sdk
